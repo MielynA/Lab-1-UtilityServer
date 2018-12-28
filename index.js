6a712790bbe38955f981eaacb3cb76a4db4a75d6
Mielyn Acosta <mielynacosta@pursuit.org>
2018-12-27 23:01:10 -0500

    finally done in add

--- index.js
+++ index.js
@@ -2,51 +2,87 @@ const express = require('express');
 const math = require('./math');
 //const giphy = require('./giphy');
 const app = express();
-//const port = 3000
 //-- Main page 
-app.get('/', (req,res)=>{
-   console.log("hey we got this Mie");
-   res.json({
-       "message": ' you are on the main page index (utility server)',
-   });
+app.get('/', (req, res) => {
+    console.log("hey we got this Mie");
+    res.json({
+        "message": ' you are on the main page index (utility server)',
+    });
 });
 //-- Math page
-app.get('/math', (req,res)=>{
-    console.log("hey we got this Mie");
+app.get('/math', (req, res) => {
     res.json({
         "message": ' you are on the math page',
     });
- });
+});
 //-- Math add page
-app.get('/math/add', (req,res)=>{
-    //const {query} = req;
+app.get('/math/add', (req, res) => {
+    let sum = 0;
+    let sumString = '';
+    const {query} = req;
+    let keyValue = Object.values(query)
+    for (let i = 0; i < keyValue.length; i++) {
+        const KeyValueI = keyValue[i] 
+        sum += parseInt(KeyValueI);
+        if (isNaN(KeyValueI)) {
+            res.json({
+                'error!': 'You passed a non-number value into the parameters.'
+            })
+            return;
+        } else if( i != keyValue.length -1) {
+             sumString += KeyValueI + ' + ';
+
+        }else {
+            sumString += KeyValueI;
+        }
+    }
+    res.json({
+        input: query,
+        sumString: sumString,
+        sum: sum,
+    })
+
+});
+//-- Math subtract page
+app.get('/math/subtract', (req, res) => {
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
-    const result = math.add(a,b);
+    const input = math.subtract(a, b);
     const returnObj = {};
-    returnObj.result = result;
+    returnObj.input = input;
 
-    res.json(returnObj);
+    res.json({
+        input
+    });
 });
+//-- Math multiply page
+app.get('/math/multiply', (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const multiply = math.multiply(a, b);
+    const input = {};
+    input.multiply = multiply;
 
-app.get('/math/subtract', (req,res)=>{
-   const a = parseInt(req.query.a);
-   const b = parseInt(req.query.b);
-   const subtract = math.subtract(a,b);
-   const returnObj = {};
-   returnObj.subtract = subtract;
+    res.json({
+        input
+    });
 
-   res.json({returnObj});
+
 });
+//-- Math divide page
 
-app.get('/math/multiply', (req,res)=>{
-   const a = parseInt(req.query.a);
-   const b = parseInt(req.query.b);
-   const multiply = math.multiply(a,b); 
-   const input = {};
-   input.multiply = multiply;
+app.get('/math/divide', (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const divide = math.divide(a, b);
+    const input = {};
+    input.divide = divide;
+    input.a = a;
+    input.b = b;
 
-   res.json({input})
+    res.json({
+        input
+    }, );
 
 
 });
